5a834c794345d785960ca94e9676741203ece6fc
Nicholas Hulston <nicholashulston@gmail.com>
2026-01-28 23:14:20 -0500

    bump version to 1.7.1

--- src/main/java/com/nhulston/essentials/Essentials.java
+++ src/main/java/com/nhulston/essentials/Essentials.java
@@ -60,7 +60,7 @@ import com.nhulston.essentials.util.VersionChecker;
 import javax.annotation.Nonnull;
 
 public class Essentials extends JavaPlugin {
-    public static final String VERSION = "1.7.0";
+    public static final String VERSION = "1.7.1";
     
     private static Essentials instance;
     
